115fe0d1573f45bb5c3925a83645c32b708d03b9
Dmytro Avruschenko <trippyfren@gmail.com>
2024-07-16 15:56:59 +0300

    Upd google_translate.py

--- utils/google_translate.py
+++ utils/google_translate.py
@@ -1,33 +1,47 @@
-from pygoogletranslation import Translator
+import time
+
+from googletrans import Translator
+from fake_useragent import UserAgent
 import csv
 
 from tqdm import tqdm
 
-translator = Translator()
+ua = UserAgent()
+translator = Translator(
+    user_agent=ua.random,
+    service_urls=['translate.google.com',]
+)
 
 
 def translate_csv(input_path, output_path):
     # Открываем .csv файл для чтения и новый .csv файл для записи результатов
-    with open(input_path, 'r', encoding='Windows-1251') as input_file, open(output_path, 'w', encoding='Windows-1251', newline='') as output_file:
+    with open(input_path, 'r', encoding='utf-8') as input_file, open(output_path, 'w', encoding='utf-8', newline='') as output_file:
         csv_reader = csv.reader(input_file, delimiter=";")
         csv_writer = csv.writer(output_file, delimiter=";")
         # Читаем каждую строку из входного файла
         for row in tqdm(csv_reader):
-            # Текст для перевода находится в первом столбце каждой строки
-            text_to_translate = str(row[0])
+            try:
+                # Текст для перевода находится в первом столбце каждой строки
+                text_to_translate = str(row[0])
 
-            # Переводим текст
-            translated_text = translator.translate(text_to_translate, dest="uk").text
+                # Переводим текст
+                translated_text = translate_text(text_to_translate)
 
-            # Записываем переведенный текст в выходной файл
-            csv_writer.writerow([translated_text.replace("\u200B", ""), row[1]])
+                # Записываем переведенный текст в выходной файл
+                csv_writer.writerow([translated_text, row[1]])
+
+            except Exception as ex:
+                print(ex)
+                time.sleep(20)
+                continue
+
 
     print(f'Перевод завершен и сохранен в {output_path}')
 
 
 def translate_text(text_to_translate):
     # Переводим текст
-    translated_text = translator.translate(text_to_translate, dest="en").text
+    translated_text = translator.translate(text_to_translate, dest="uk").text
     return translated_text
 
 
@@ -38,7 +52,7 @@ def translate_corpus(corpus_to_translate):
 
 
 if __name__ == '__main__':
-    INPUT_PATH = '../datasets/propaganda_on_article_level.csv'
-    OUTPUT_PATH = '../datasets/propaganda_on_article_level_ua.csv'
+    INPUT_PATH = '../datasets/propaganda_on_sentence_level.csv'
+    OUTPUT_PATH = '../datasets/propaganda_on_sentence_level_ua_not_provided.csv'
 
     translate_csv(INPUT_PATH, OUTPUT_PATH)
